5994e181c8b9fa15d1c38c6759f774ca42a3a7c5
Abu Talha <itsabutalha@gmail.com>
2023-04-13 11:13:33 +0530

    refactor code

--- src/config/database.js
+++ src/config/database.js
@@ -1,18 +1,21 @@
 const mongoose = require("mongoose");
 
 exports.connectDatabase = () => {
-  const DB_URI =
+  const databaseName =
     process.env.NODE_ENV === "test"
-      ? process.env.MONGO_URI_TEST
-      : process.env.MONGO_URI_DEV;
+      ? process.env.TEST_DB_NAME
+      : process.env.DEV_DB_NAME;
 
   const options = {
+    dbName: databaseName,
+    user: process.env.DB_USER,
+    pass: process.env.DB_PASS,
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  };
+    };
 
   mongoose
-    .connect(DB_URI, options)
+    .connect(process.env.MONGO_URI, options)
     .then((con) => console.log(`Database Connected: ${con.connection.host}`))
     .catch((err) => console.log(err));
 };
